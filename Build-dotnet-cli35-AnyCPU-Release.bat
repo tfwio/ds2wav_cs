62dc8ce4bdc5f06b786a807395c6b462c68f87a6
tfwio <tfwroble@gmail.com>
2019-05-30 10:27:21 -0500

    add build scripts

---
+++ Build-dotnet-cli35-AnyCPU-Release.bat
@@ -0,0 +1,6 @@
+#! cmd.exe /c
+@echo off
+REM set msbuild_path=C:\Program Files (x86)\msbuild\14.0\bin
+set msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+set PATH=%PATH%;%msbuild_path%
+msbuild /m "ide\\ds2wav.sln" "/t:ds2wav" "/p:Platform=Any CPU;Configuration=Release"
